53be66dfded879fc31b3eb3f0d95c25641f7c1f9
DeclanAS <58081720+DeclanAS@users.noreply.github.com>
2019-12-10 11:48:15 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -39,6 +39,7 @@ struct Edge
         return lhs.weight < rhs.weight;
     }
     */
+    
 };
 
 class Graph
@@ -51,6 +52,7 @@ public:
      * 
      * Guassian Blur: https://en.wikipedia.org/wiki/Gaussian_blur
      */
+    
     void SegmentImage(cv::Mat &image, int c, int min_size);
 
     /* For the diff() function we used opencv: 'cv::Vec3f', 'cv::Mat'
@@ -60,24 +62,29 @@ public:
     * 
     * finds diff of two pixels by performing the euclidian diastance algolrithm using dot product
     */
+    
     float diff(const cv::Mat &image, int q1, int p1, int q2, int p2);
 
     /* Builds the graph
     * References 'Huttenlocher' paper on construction of pixels into the graph
     * This also relies of the datastructure cv::Mat for loading the image pixels
     */
+    
     std::vector<Edge> build_graph(const cv::Mat &image);
 
     /* Builds the segmented graph
     * References 'Huttenlocher' paper on disjoint sets and forest
     */
+    
     Disjoint segment_graph(int num_vertices, std::vector<Edge> &edges, float c);
 
 private:
+    
     /* Takes the image plus a disjoint set forest
      * recolors all of the pixels inside of the disjoin set
      * to the color of the average of all the pixels on that threshold range
      */
+    
     void ForestColor(cv::Mat &image, Disjoint &forest);
 };
 
